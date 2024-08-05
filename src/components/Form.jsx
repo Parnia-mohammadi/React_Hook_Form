de8c424f42416c5e112ed1bbf004a59b6ca9df1b
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2024-08-06 01:36:47 +0330

    form created by html

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,20 @@
 function Form() {
   return (
-    <>
-      <p className="text-5xl text-white">form</p>
-    </>
+    <form>
+      <label htmlFor="name">First Name</label>
+      <input required type="text" id="name" />
+      <label htmlFor="lastName">Last Name</label>
+      <input required type="text" id="lastName" />
+      <input type="email" name="Email" id="email" />
+      <label htmlFor="query"></label>
+      <input type="radio" name="general" id="query" />
+      <input type="radio" name="support" id="query" />
+      <label htmlFor="message">Message</label>
+      <input type="text" name="message" id="message" />
+      <input type="checkbox" name="consent" id="consent" />
+      <label htmlFor="consent">I consent to being contacted by the team</label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
