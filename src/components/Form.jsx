4ec92fe46857e010d7d0ac4d4be69be0204d7601
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2024-08-11 18:54:13 +0330

    html of the form is built

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,34 @@
 function Form() {
   return (
-    <form>
-      <label htmlFor="name">First Name</label>
-      <input required type="text" id="name" />
-      <label htmlFor="lastName">Last Name</label>
-      <input required type="text" id="lastName" />
-      <input type="email" name="Email" id="email" />
-      <label htmlFor="query"></label>
-      <input type="radio" name="general" id="query" />
-      <input type="radio" name="support" id="query" />
-      <label htmlFor="message">Message</label>
-      <input type="text" name="message" id="message" />
-      <input type="checkbox" name="consent" id="consent" />
-      <label htmlFor="consent">I consent to being contacted by the team</label>
-      <input type="submit" value="Submit" />
-    </form>
+    <>
+      <p>Contact Us </p>
+      <form id="form">
+        <label htmlFor="name">First Name </label>
+        <input required type="text" id="name" />
+        <label htmlFor="lastName">Last Name </label>
+        <input required type="text" id="lastName" />
+        <label htmlFor="email">Email </label>
+        <input required type="email" name="Email" id="email" />
+        <p>Query Type </p>
+        <input
+          required
+          type="radio"
+          name="query"
+          id="general"
+          value="General Enquery"
+        />
+        <label htmlFor="general">General Enquery</label>
+        <input required type="radio" name="query" id="support" />
+        <label htmlFor="support">Support Request</label>
+        <label htmlFor="message">Message</label>
+        <input required type="text" name="message" id="message" />
+        <input required type="checkbox" name="consent" id="consent" />
+        <label htmlFor="consent">
+          I consent to being contacted by the team
+        </label>
+        <input required type="submit" value="Submit" />
+      </form>
+    </>
   );
 }
 
