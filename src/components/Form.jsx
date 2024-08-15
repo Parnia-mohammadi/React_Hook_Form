4ba6a884f52e6cf03160cd9cc83aba65d59b5252
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2024-08-15 18:02:57 +0330

    form styles added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,61 +2,85 @@ function Form() {
   return (
     <div
       className="w-1/2 border-black border-solid border-1 rounded-lg
-     bg-white flex-col justify-center items-center p-[3%]"
+     bg-white flex-col justify-center items-center p-[3%] gap-y-4"
     >
-      <h1 className="text-xl font-bold">Contact Us </h1>
-      <form id="form" className="flex flex-col justify-center items-center">
-        <div>
+      <h1 className="text-xl font-bold my-4">Contact Us </h1>
+      <form id="form" className="flex flex-wrap gap-x-2 gap-y-2 ">
+        {/* FirstName */}
+        <div className="flex-1 w-full">
           <label htmlFor="name">First Name *</label>
           <br />
           <input
             required
             type="text"
             id="name"
-            className="border-black border-1 border-solid rounded-md"
+            className="w-full border-gray-400 border-[1px] border-solid rounded-md"
           />
+        </div>
+        {/* LastName */}
+        <div className="flex-1 w-full">
           <label htmlFor="lastName">Last Name *</label>
           <br />
           <input
             required
             type="text"
             id="lastName"
-            className="w-1/2 border-black border-1 border-solid rounded-md"
+            className="w-full border-gray-400 border-[1px] border-solid rounded-md"
           />
         </div>
-        <div>
+        {/* email */}
+        <div className=" w-full">
           <label htmlFor="email">Email Address *</label>
-          <input required type="email" name="Email" id="email" />
-        </div>
-        <div>
-          <p>Query Type *</p>
           <input
             required
-            type="radio"
-            name="query"
-            id="general"
-            value="General Enquery"
+            type="email"
+            name="Email"
+            id="email"
+            className="w-full border-gray-400 border-[1px] border-solid rounded-md"
           />
-          <label htmlFor="general">General Enquery</label>
-          <input required type="radio" name="query" id="support" />
-          <label htmlFor="support">Support Request</label>
         </div>
-        <div>
-          <label htmlFor="message">Message</label>
+        {/* query */}
+        <p className=" w-full">Query Type *</p>
+        <div className="flex flex-row justify-start items-center flex-nowrap flex-1 gap-x-4">
+          <div className="w-1/2 border-[1px] border-gray-400 border-solid rounded-md p-4">
+            <input
+              required
+              type="radio"
+              name="query"
+              id="general"
+              value="General Enquery"
+            />
+            <label htmlFor="general">General Enquery</label>
+          </div>
+          <div className="w-1/2 border-[1px] border-gray-400 border-solid rounded-md p-4">
+            <input required type="radio" name="query" id="support" />
+            <label htmlFor="support">Support Request</label>
+          </div>
+        </div>
+        {/* Message */}
+        <div className=" w-full mb-4">
+          <label htmlFor="message">Message *</label>
           <br />
-          <input required type="text" name="message" id="message" />
+          <input
+            required
+            type="text"
+            name="message"
+            id="message"
+            className="border-gray-400 border-[1px] border-solid rounded-md h-full w-full"
+          />
         </div>
-        <div>
+        {/* checkbox */}
+        <div className=" w-full my-4">
           <input required type="checkbox" name="consent" id="consent" />
           <label htmlFor="consent">
-            I consent to being contacted by the team
+            <span> I consent to being contacted by the team</span>
           </label>
         </div>
         <input
           required
           type="submit"
           value="Submit"
-          className="bg-emerald-700 text-white w-full rounded-lg"
+          className="bg-emerald-700 text-white w-full rounded-lg py-[3%]"
         />
       </form>
     </div>
